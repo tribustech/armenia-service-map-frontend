bbdd039b78bc80668038975cdd088cba5811de83
Andrew Radulescu <radulescuandrew@gmail.com>
2026-03-25 10:06:42 +0200

    feat: wire header and footer into public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,8 +1,12 @@
+import { PublicHeader } from '@/components/public/header';
+import { PublicFooter } from '@/components/public/footer';
+
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {/* Public header and footer will be added in Phase 5 */}
+    <div className="flex min-h-screen flex-col">
+      <PublicHeader />
       <main className="flex-1">{children}</main>
-    </>
+      <PublicFooter />
+    </div>
   );
 }
